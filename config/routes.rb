619c24a50ce363845babe905640586e87398f2f6
chenyunli <m18301662790@gmail.com>
2020-01-10 20:24:09 +0800

    private api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "welcome#index"
+
   resources :cities do
     member do
       post :update_temp
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
       resources :trains, :only => [:index, :show]
-      resources :reservations, :only => [:show, :create, :update, :destroy]
+      resources :reservations, :only => [:index, :show, :create, :update, :destroy]
     end
   end
 end
