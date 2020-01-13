963107dfcbaca40e7d6702b5d4f78e30d9a8e282
chenyunli <m18301662790@gmail.com>
2020-01-13 14:33:10 +0800

    me api

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :reservations
 
   before_create :generate_authentication_token
+
+  mount_uploader :avatar, AvatarUploader
   
   def generate_authentication_token
     self.authentication_token = Devise.friendly_token
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,13 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
+
+      get "/me" => "users#show", :as => :user
+      patch "/me" => "users#update", :as => :update_user
+
       resources :trains, :only => [:index, :show]
       resources :reservations, :only => [:index, :show, :create, :update, :destroy]
     end
